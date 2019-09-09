c95e076f5fd4cf800ce26afd35be170e89730c1c
Douglas Nickson <douglas.nickson1@gmail.com>
2019-09-08 23:12:28 -0300

    Mudança da API de envio de email

--- src/index.js
+++ src/index.js
@@ -3,32 +3,66 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const sgMail = require('@sendgrid/mail');
+const mailgun = require('mailgun-js');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/enviar-email', (req, res) => {
-    res.send("API Iniciada...");
-    enviarEmail(req);
+  res.send("API Iniciada...");
+  enviarEmail(req);
 });
 
 app.listen(process.env.PORT || 3000);
 
 async function enviarEmail(req) {
-    sgMail.setApiKey(process.env.SENDGRID_KEY);
-    const msg = {
-        from: `${req.query.name} <${req.query.from}>`,
-        to: `${process.env.EMAIL_TO}`,
-        subject: `${req.query.subject}`,
-        text: `${req.query.text}`,
-        html: `<b>${req.query.html}</b>`
-    }
-    sgMail.send(msg).then((sent) => {
-        console.log('Email enviado com sucesso!');
-        console.log(sent.statusCode);
-    }).catch((error) => {
-        console.log("Ocorreu um erro ao enviar o email.")
-        console.log(error);
-    });
+
+  const mailjet = require('node-mailjet').connect(process.env.MAILJET_API_KEY, process.env.MAILJET_API_SECRET);
+  const request = mailjet
+    .post("send", { 'version': 'v3.1' })
+    .request({
+      "Messages": [
+        {
+          "From": {
+            "Email": `${req.query.from}`,
+            "Name": `${req.query.name}`
+          },
+          "To": [
+            {
+              "Email": `${process.env.EMAIL_TO}`,
+              "Name": "MinhaPrevidencia"
+            }
+          ],
+          "Subject": "MinhaPrevidência - Relatório Aplicativo",
+          "TextPart": "Nova Submissão de Relatório",
+          "HTMLPart": `${req.query.html}`,
+          "CustomID": "AppMinhaPrevidencia"
+        }
+      ]
+    })
+  request
+    .then((result) => {
+      console.log(result.body);
+    })
+    .catch((err) => {
+      console.log(err.statusCode);
+    })
+
+
+  // sgMail.setApiKey(process.env.SENDGRID_KEY);
+  // const msg = {
+  //     from: `${req.query.name} <${req.query.from}>`,
+  //     to: `${process.env.EMAIL_TO}`,
+  //     subject: `${req.query.subject}`,
+  //     text: `${req.query.text}`,
+  //     html: `<b>${req.query.html}</b>`
+  // }
+  // sgMail.send(msg).then((sent) => {
+  //     console.log('Email enviado com sucesso!');
+  //     console.log(sent.statusCode);
+  // }).catch((error) => {
+  //     console.log("Ocorreu um erro ao enviar o email.")
+  //     console.log(error);
+  // });
 }
